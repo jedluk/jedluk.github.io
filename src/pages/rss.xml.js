6fbd40a37b0ca7ddd34f029f862793767afb7257
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-10-08 21:23:47 +0200

    add image to feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,27 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection('blog')
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
+    items: posts.map((post) => ({
+      ...post.data,
+      author: 'jedrzej.lukasiuk@gmail.com',
+      link: `/blog/${post.slug}/`,
+      customData: `<media:content
+        type="image/${/\.jpe?g/.test(post.data.heroImage) ? 'jpeg' : 'png'}"
+        width="320"
+        height="440"
+        medium="image"
+        url="${post.data.heroImage}" />
+      `,
+    })),
+  })
 }
