2f5b262b4cd3558de774ab25aa4864bb48ac4b61
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-10-13 13:01:50 +0200

    Add react integration, post skeleton

---
+++ src/components/blog-samples/ExportCSV.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import { useExportableCSV } from 'use-exportable-csv'
+
+export default function CSVLink() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then(setData)
+  }, [])
+
+  const options = { bom: true }
+  const csvLink = useExportableCSV(data, options)
+
+  return (
+    <a className="hover:text-white" href={csvLink} download="data.csv">
+      CSV download
+    </a>
+  )
+}
