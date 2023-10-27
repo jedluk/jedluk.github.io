3b178e23e6057d8997b5f467a2ddf2b6a361ed3b
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-10-27 20:29:16 +0200

    Tailwind & static assets improvements (#29)
    * custom tailwind colors
    
    * clean up static assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        accent: 'var(--accent)',
+      },
       animation: {
         tech: 'flow 25s linear infinite',
       },
