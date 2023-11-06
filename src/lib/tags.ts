9c009824ae172c5eb3da6d7916266d1ec7ef6f0f
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-06 13:19:52 +0100

    simplify return from getTagsByCount

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -4,10 +4,19 @@ export function extractTags(post: CollectionEntry<'blog'>): string[] {
     return post.data.tags.map(tag => tag.toLowerCase())
 }
 
-export function getTagsByCount(posts: CollectionEntry<'blog'>[]): Map<string, number> {
-    return posts
+// sort unique tags by count first and then by name
+export function getTagsByCount(posts: CollectionEntry<'blog'>[]): [string, number][] {
+    const tagsCountMap = posts
         .map(extractTags)
         .flat()
         .reduce((acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1)
             , new Map<string, number>())
+
+    return Array
+        .from(tagsCountMap.entries())
+        .sort(([name1, count1], [name2, count2]) =>
+            count1 === count2
+                ? (name1 > name2 ? 1 : -1)
+                : (count2 > count1 ? 1 : -1)
+        )
 }
