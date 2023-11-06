de8b50973ec2fb9ce5e0b7578c58c7488353575c
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-06 15:46:56 +0100

    Dark theme based on body class instead data attribute (#38)

--- public/loadTheme.js
+++ public/loadTheme.js
@@ -1,17 +1,9 @@
 const LS_KEY = 'theme'
-let theme = 'light'
-
-const isValidTheme = (theme) =>
-  theme !== null && ['light', 'dark'].includes(theme)
 
 const item = window.localStorage.getItem(LS_KEY)
-if (isValidTheme(item)) {
-  theme = item
-} else {
-  window.localStorage.removeItem(LS_KEY)
-}
+const isDarkPreffered =
+  item === null && window.matchMedia('(prefers-color-scheme: dark)').matches
 
-const body = document.querySelector('body')
-if (body !== null) {
-  body.dataset.theme = theme
+if (item === 'dark' || isDarkPreffered) {
+  document.querySelector('body').classList.add('dark')
 }
