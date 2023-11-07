e27b2b14af0a06c9ca042241edcfd9097d769d0a
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-07 10:42:12 +0100

    format code

--- scripts/const.cjs
+++ scripts/const.cjs
@@ -2,5 +2,5 @@ const { join } = require('path')
 
 module.exports = {
   BLOG_ASSETS_DIR: join(__dirname, '..', 'public', 'blog'),
-  BLOG_CONTENT_DIR: join(__dirname, '..', 'src', 'content', 'blog'),
+  BLOG_CONTENT_DIR: join(__dirname, '..', 'src', 'content', 'blog')
 }
--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -1,22 +1,23 @@
-import type { CollectionEntry } from "astro:content"
+import type { CollectionEntry } from 'astro:content'
 
 export function extractTags(post: CollectionEntry<'blog'>): string[] {
-    return post.data.tags.map(tag => tag.toLowerCase())
+  return post.data.tags.map((tag) => tag.toLowerCase())
 }
 
 // sort unique tags by count first and then by name
-export function getTagsByCount(posts: CollectionEntry<'blog'>[]): [string, number][] {
-    const tagsCountMap = posts
-        .map(extractTags)
-        .flat()
-        .reduce((acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1)
-            , new Map<string, number>())
+export function getTagsByCount(
+  posts: CollectionEntry<'blog'>[]
+): [string, number][] {
+  const tagsCountMap = posts
+    .map(extractTags)
+    .flat()
+    .reduce(
+      (acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1),
+      new Map<string, number>()
+    )
 
-    return Array
-        .from(tagsCountMap.entries())
-        .sort(([name1, count1], [name2, count2]) =>
-            count1 === count2
-                ? (name1 > name2 ? 1 : -1)
-                : (count2 > count1 ? 1 : -1)
-        )
+  return Array.from(tagsCountMap.entries()).sort(
+    ([name1, count1], [name2, count2]) =>
+      count1 === count2 ? (name1 > name2 ? 1 : -1) : count2 > count1 ? 1 : -1
+  )
 }
