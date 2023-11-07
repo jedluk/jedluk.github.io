d5f43b66f8ba1ba4e0d4c5bc40ae918cb2076902
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-07 14:37:07 +0100

    create ThemeIcon component

--- public/loadTheme.js
+++
@@ -1,9 +0,0 @@
-const LS_KEY = 'theme'
-
-const item = window.localStorage.getItem(LS_KEY)
-const isDarkPreffered =
-  item === null && window.matchMedia('(prefers-color-scheme: dark)').matches
-
-if (item === 'dark' || isDarkPreffered) {
-  document.querySelector('body').classList.add('dark')
-}
