19305bf20733eb52f3f6d31fb4369d7d7cbc2ba1
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-07 11:46:45 +0100

    run format

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -11,8 +11,10 @@ export function getTagsByCount(posts: CollectionEntry<'blog'>[]): TagInfo[] {
   const tagsCountMap = posts
     .map(extractTags)
     .flat()
-    .reduce((acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1)
-      , new Map<string, number>())
+    .reduce(
+      (acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1),
+      new Map<string, number>()
+    )
 
   return Array.from(tagsCountMap.entries()).sort(
     ([name1, count1], [name2, count2]) =>
