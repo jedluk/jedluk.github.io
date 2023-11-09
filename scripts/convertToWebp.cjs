99cd6597eeb2c56f2201071b4a809dab94b02ea5
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-11-09 14:30:45 +0100

    update scripts for transforming images

--- scripts/convertToWebp.cjs
+++ scripts/convertToWebp.cjs
@@ -8,12 +8,27 @@ const images = readDir(BLOG_ASSETS_DIR, extensions)
 
 console.log(`${images.length} images to be transformed.`)
 for (const image of images) {
-  const newFileName = basename(image).replace(extname(image), '.webp')
-  const output = join(dirname(image), newFileName)
+  const baseFileName = basename(image).replace(extname(image), '')
 
-  sharp(image).toFile(output, (err, info) => {
-    if (!err) {
-      console.log(`${basename(image)} transformed`)
-    }
+  const SIZE_TO_WIDTH = {
+    small: 600,
+    medium: 900,
+    large: undefined // no width change
+  }
+
+  Object.keys(SIZE_TO_WIDTH).forEach(async (size) => {
+    const fileName = `${baseFileName}_${size}.webp`
+    const output = join(dirname(image), fileName)
+
+    await sharp(image)
+      .resize(SIZE_TO_WIDTH[size])
+      .toFile(output)
+      .then((data) => {
+        console.log(`Generated ${fileName}`)
+      })
+      .catch((err) => {
+        console.err(err)
+        console.log(`Cannot transform ${image} into ${fileName}`)
+      })
   })
 }
--- scripts/replaceImagesSource.cjs
+++ scripts/replaceImagesSource.cjs
@@ -2,7 +2,7 @@ const { BLOG_CONTENT_DIR } = require('./const.cjs')
 const readDir = require('./readDir.cjs')
 const fs = require('fs')
 
-// Create a regular expression to match <img /> tags and capture the src attribute and everything between the <img> and />
+// Regular expression to match <img /> tags and capture the src attribute and everything between the <img and />
 const IMG_SRC_REGEX = /<img\b(.*?)src=['"](.*?)['"](.*?)\/?>/g
 const extensions = ['md', 'mdx']
 
@@ -13,10 +13,12 @@ posts.forEach((post) => {
 
   const updatedContent = fileContent.replace(
     IMG_SRC_REGEX,
-    (match, beforeSrc, src, afterSrc) => {
+    (match, beforeSrc, imgSrc, afterSrc) => {
       console.log(`transforming ${match}`)
-      const webpSrc = src.replace(/\.(jpe?g|png)$/, '.webp')
-      return `<img${beforeSrc}src="${webpSrc}"${afterSrc}/>`
+      const basename = imgSrc.slice(0, imgSrc.lastIndexOf('.'))
+      const src = `${basename}_small.webp`
+      const srcset = `${basename}_medium.webp 600w, ${basename}_large.webp 900w`
+      return `<img${beforeSrc}src="${src}" srcset="${srcset}"${afterSrc}/>`
     }
   )
 
